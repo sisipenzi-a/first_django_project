b47b7cd0d84c4ae90a23537080ca106e5b622762
sisipenzi-a <andrew.sisipenzi@students.uz.ac.zw>
2022-10-29 03:39:01 +0200

    Added Models to the musicapp.

--- songcrud/musicapp/admin.py
+++ songcrud/musicapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from . import models
+
+class MusicappAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(models.Artiste, MusicappAdmin)
+admin.site.register(models.Song, MusicappAdmin) 
+admin.site.register(models.Lyric, MusicappAdmin)
--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Artiste(models.Model):
+   first_name = models.CharField(max_length=20)
+   last_name = models.CharField(max_length=20)
+   age = models.IntegerField() 
+   
+   def __str__(self):
+       return self.last_name + "\n" + self.first_name
+   
+class Song(models.Model):
+    #foreign key from Artiste
+    title = models.CharField(max_length=30)
+    date_released = models.DateField()
+    likes = models.IntegerField(null=True)
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE, related_name='song')
+    
+    def __str__(self):
+        return self.title
+    
+class Lyric(models.Model):
+    #foreign key from Song
+    content = models.TextField()
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE, related_name='lyric')
+    
+    def __str__(self):
+        return self.content
